631ee469121103e15952dd6c2f2d9cb87ceb0c5d
David Wu <YusakuNo1@hotmail.com>
2023-10-06 00:59:43 -0700

    Added chromadb dependency and change vector store folder name

--- src/pf_copilot/consts.py
+++ src/pf_copilot/consts.py
@@ -1,4 +1,4 @@
-persist_directory = "./.db"
+persist_directory = "./vectorstore"
 chat_model_deployment = "gpt-35-turbo-16k"
 embeddings_model_deployment = "text-davinci-002" 
 model_name = "gpt-35-16k-turbo"
