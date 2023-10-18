7afad5b3ab2e8d27c9e29fc3f7e9db812b17a36d
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-10-18 14:21:57 -0400

    fix up invoke deployment chat script

--- src/invoke_deployment_chat.py
+++ src/invoke_deployment_chat.py
@@ -4,11 +4,12 @@ import os
 
 headers = {
     "Content-Type": "application/json",
+    "Authorization": f"Bearer {os.getenv('DEPLOYMENT_KEY')}",
     "Accept": "text/event-stream",
 }
 
 response = requests.post(
-    "http://127.0.0.1:5001/score",
+    os.getenv("DEPLOYMENT_SCORING_URL"),
     headers=headers,
     json={
         "messages": [{"role": "user", "content": "What tent has the highest rainfly rating?"}],
