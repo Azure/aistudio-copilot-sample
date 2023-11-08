a9c35d6bf5ba28f52996de5121dff6fdd879d007
Neehar Duvvuri <40341266+needuv@users.noreply.github.com>
2023-11-08 14:17:51 -0500

    fix invoke script

--- src/foundation_model/azureml/invoke.py
+++ src/foundation_model/azureml/invoke.py
@@ -6,17 +6,18 @@ from argparse import ArgumentParser
 
 from azure.ai.resources.client import AIClient
 from azure.ai.resources.entities.deployment import Deployment
-from azure.identity import DefaultAzureCredential as Credential
+from azure.identity import InteractiveBrowserCredential as Credential
 
 def invoke(deployment_name: str):
     credential = Credential()
 
     client = AIClient.from_config(credential)
 
-    client.deployments.invoke(deployment_name, "./request_file.json")
+    return client.deployments.invoke(deployment_name, "./request_file.json")
 
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--deployment-name", help="Name of the deployment to invoke")
     parsed_args = parser.parse_args()
-    invoke(parsed_args.deployment_name)
+    result = invoke(parsed_args.deployment_name)
+    print(result)
