a4e0754f4bf82087c22dfcf0f1f9c3ec4debf8a5
Matthew Bolaños <matthewbolanos@gmail.com>
2023-11-13 23:25:09 +0000

    Add streaming response to SK implementation

--- src/copilot_semantickernel/chat.py
+++ src/copilot_semantickernel/chat.py
@@ -3,14 +3,21 @@ from __future__ import annotations
 import datetime
 
 import os
+import pathlib
 from typing import Any
+import jinja2
+import openai
 
 import semantic_kernel as sk
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.planning import StepwisePlanner
 from semantic_kernel.planning.stepwise_planner.stepwise_planner_config import StepwisePlannerConfig
 from copilot_semantickernel.plugins.customer_support_plugin.customer_support import CustomerSupport
+from streaming_utils import add_context_to_streamed_response
 
+templateLoader = jinja2.FileSystemLoader(pathlib.Path(__file__).parent.resolve())
+templateEnv = jinja2.Environment(loader=templateLoader)
+system_message_template = templateEnv.get_template("system-message.jinja2")
 
 async def chat_completion(messages: list[dict], stream: bool = False, 
     session_state: Any = None, context: dict[str, Any] = {}):
@@ -30,7 +37,7 @@ async def chat_completion(messages: list[dict], stream: bool = False,
     )
 
     customer_support_plugin = CustomerSupport(
-        number_of_docs = context.get("num_retrieved_docs", 3),
+        number_of_docs = context.get("num_retrieved_docs", 10),
         embedding_model_deployment = os.environ["AZURE_OPENAI_EMBEDDING_MODEL"],
         chat_model_deployment=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT"),
         temperature=context.get("temperature", 0.7)
@@ -49,18 +56,26 @@ async def chat_completion(messages: list[dict], stream: bool = False,
     result = await kernel.run_async(plan)
 
     # limit size of returned context
-    context = customer_support_plugin.context
+    planner_context = customer_support_plugin.context
     if len(context) > 40000:
-        context = context[:40000]
+        planner_context = context[:40000]
 
-    return {
-        "choices": [{
-            "index": 0,
-            "message": {
-                "role": "assistant",
-                "content": result.result
-            },
-            "context": context,
-        }]
-    }
+
+    # add retrieved documents as context to the system prompt
+    system_message = system_message_template.render(answer=result["input"])
+    messages.insert(0, {"role": "system", "content": system_message})
+
+    # call Azure OpenAI with the system prompt and user's question
+    response = openai.ChatCompletion.create(
+        engine=os.environ.get("AZURE_OPENAI_CHAT_DEPLOYMENT"),
+        messages=messages, temperature=context.get("temperature", 0.7),
+        stream=stream,
+        max_tokens=800)
+
+    # add context in the returned response
+    if not stream:
+        response.choices[0]['context'] = planner_context
+    else:
+        response = add_context_to_streamed_response(response, planner_context)
+    return response
 
